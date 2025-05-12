1be6782d57530f9efaa5d9661ed28d3f83a9bb43
ElvDev05 <vasquezelverth@gmail.com>
2025-05-12 12:50:05 -0500

    feat: first commit

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+ PRUEBA
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,50 @@
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import './style.css'
+import App from './app.vue'
+import {
+    Button, Card, Checkbox,
+    Column,
+    ConfirmationService,
+    ConfirmDialog,
+    DataTable, Dialog,
+    DialogService, FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu, Rating,
+    Row, Tag, Textarea, Toast,
+    ToastService, Toolbar
+} from "primevue";
+import router from "./router/index.js";
+
+createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    })
+    .use(router)
+    .use(PrimeVue, {ripple: true})
+    .use(ToastService)
+    .use(DialogService)
+    .use(ConfirmationService)
+    .component('pv-button', Button)
+    .component('pv-data-table', DataTable)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-row', Row)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-input-text', InputText)
+    .component('pv-textarea', Textarea)
+    .component('pv-menu', Menu)
+    .component('pv-dialog', Dialog)
+    .component('pv-toast', Toast)
+    .component('pv-tag', Tag)
+    .component('pv-card', Card)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-number', InputNumber)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-rating', Rating)
+    .mount('#app')
